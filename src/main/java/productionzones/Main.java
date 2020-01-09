2d2e8a13cd768f7530d20e292f7c9753edb81aaa
Arno <gandalf2.ac@gmail.com>
2020-01-09 18:30:44 +0100

    Create Main.java

---
+++ src/main/java/productionzones/Main.java
@@ -0,0 +1,24 @@
+package productionzones;
+
+@Mod(modid="ProductionZones", version="1.0.0", serverSideOnly = true, acceptableRemoteVersions="*")
+
+public class Main
+{
+public static String MODID = "modid";
+public static String VERSION = "version";
+
+@EventHandler
+public void preInit(FMLPreInitializationEvent e)
+{
+}
+
+@EventHandler
+public void init(FMLInitializationEvent e)
+{
+}
+
+@EventHandler
+public void postInit(FMLPostInitializationEvent e)
+{
+}
+}
